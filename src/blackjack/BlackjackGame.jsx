b2254c3e365ad40c82c08e4ecbee5bc96a93b268
Kang Yiran <121548100+Saturn-22@users.noreply.github.com>
2025-11-04 23:14:31 +0800

    Update BlackjackGame.jsx

--- src/blackjack/BlackjackGame.jsx
+++ src/blackjack/BlackjackGame.jsx
@@ -17,7 +17,8 @@ const userVaultAddress = USER_VAULT_ADDRESS;
 const blackjackAddress = BLACKJACK_ADDRESS;
 
 // API endpoint
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000';
+//const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000';
+const API_BASE_URL = https://blackjack-dptz.onrender.com;
 
 // --- Helper Functions ---
 
